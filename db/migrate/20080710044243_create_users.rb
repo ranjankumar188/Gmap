df029f76ea1730e683f8ffc326771e3a887135b2
ranjan kumar <ranjan@srv-530q1.gb1.brightbox.com>
2011-06-13 06:11:59 +0000

    Added migration

--- db/migrate/20080710044243_create_users.rb
+++ db/migrate/20080710044243_create_users.rb
@@ -1,7 +1,16 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
-
+      t.string :username
+      t.string :email
+      t.string :name
+      t.string :password
+      t.string :question
+      t.string :answer
+      t.string :password_salt
+      t.string :password_hash
+      t.string :status
+      t.string :email_activation
       t.timestamps
     end
   end
